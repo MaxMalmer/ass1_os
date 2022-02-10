4ff0349823e24d491d7621aa5dffde8f6167f9c6
Max Malmer <c17mmr@cs.umu.se>
2022-02-10 16:03:44 +0100

    minor updates and plots

--- assignment1/plot2.py
+++ assignment1/plot2.py
@@ -3,6 +3,8 @@ import time
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import subprocess
+import numpy as np
+
 import random
 import collections
 
@@ -14,16 +16,15 @@ FILENAME = PLOTNAME + ".png"
 
 #iteration param not used
 def run_tests(iterations):
-    for optionlist in OPTIONS:
-        print("Running program: " + PROGRAMNAME + " with options " + str(optionlist))
-        #command = ["/usr/bin/time", "-f", "%e", PROGRAMNAME] + optionlist
-        command = [PROGRAMNAME] + optionlist
-        pipe = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = pipe.communicate()
-        out_lines = stdout.decode("utf-8").splitlines()
-        err_lines = stderr.decode("utf-8").splitlines()
+    print("Running program: " + PROGRAMNAME + " with options ")
+    #command = ["/usr/bin/time", "-f", "%e", PROGRAMNAME]
+    command = [PROGRAMNAME]
+    pipe = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = pipe.communicate()
+    out_lines = stdout.decode("utf-8").splitlines()
+    err_lines = stderr.decode("utf-8").splitlines()
 
-        return out_lines, err_lines
+    return out_lines, err_lines
     
 
 
@@ -42,4 +43,4 @@ def make_and_save_plot(data,  plotfilename):
 out, err = run_tests(10)
 print(out)
 print(err)
-make_and_save_plot([list(range(0,len(out))), out], FILENAME)
+make_and_save_plot([range(0,600, 100), out], FILENAME)
